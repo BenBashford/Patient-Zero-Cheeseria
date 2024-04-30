16212ea6fd9d13164609f6719f2e39b2b394c936
BenBashford <75756448+BenBashford@users.noreply.github.com>
2024-04-30 16:31:03 +1000

    Initial Commit

---
+++ Cheeseria/src/main/java/Patient/Zero/Cheeseria/CheeseriaApplication.java
@@ -0,0 +1,13 @@
+package Patient.Zero.Cheeseria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseriaApplication.class, args);
+	}
+
+}
